2355490ca75159faf71effdfb2f2abf7b610e393
ehomeshasha <693946948@qq.com>
2013-12-26 16:35:33 +0800

    ed

---
+++ easydata/urls.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
+from django.views.generic import TemplateView
+
+from django.contrib import admin
+from datafinder import views
+
+urlpatterns = patterns("",
+    url(r"^$", TemplateView.as_view(template_name="homepage.html"), name="home"),
+    url(r"^admin/", include(admin.site.urls)),
+    url(r"^account/", include("account.urls")),
+    url(r'^(?P<poll_id>\d+)/vote/$', views.vote, name='vote'),
+)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
