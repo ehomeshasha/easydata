17a7ee699ae76b5dbab7c07b25b9814c78c1d6d0
ehomeshasha <693946948@qq.com>
2014-01-03 10:04:32 +0800

    ed

--- pdf/func.py
+++ pdf/func.py
@@ -16,22 +16,4 @@ def get_upload_dir(directory):
     if not os.path.exists(directory_abs):
         os.makedirs(directory_abs)
         return directory
-    if not os.path.exists(directory_abs) or not elistdir(directory_abs+'/pdf', 'file'):
-        return directory
-    user_dir = os.path.dirname(directory_abs)
-    dirname = os.path.basename(directory_abs)
-    dirlist = elistdir(user_dir, 'directory')
-    if not dirname in dirlist:
-        return directory
-    else:
-        pattern = re.compile(dirname+'_(\d+)')
-        max_num = 0
-        for subdir in dirlist:
-            match = re.search(pattern, subdir)
-            if match:
-                n = int(match.group(1)) + 1
-                if n > max_num:
-                    max_num = n
-            else:
-                continue
-        return directory+'_'+str(max_num)
+    
