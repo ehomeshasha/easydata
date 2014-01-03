5032674b0d6b11fa484e32a80bce793e4e07b584
ehomeshasha <693946948@qq.com>
2014-01-03 09:11:55 +0800

    ed

--- easydata/settings.py
+++ easydata/settings.py
@@ -18,7 +18,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         'NAME': 'easydata',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'dealsaccess',
+        'USER': 'root',
         'PASSWORD': 'zzy8945620',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
