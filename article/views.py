35c2ab2bf7e6c6a05b900e3ffcd4c8860a73ca3a
hadoop-user <hadoop-user@home.ehomeshasha>
2014-01-15 21:11:27 +0800

    ed

--- article/views.py
+++ article/views.py
@@ -3,30 +3,33 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from easydata.constant import HOME_BREAD
+from django.conf import settings
 
-
+context = {
+    'SITE_URL': settings.SITE_URL
+}
 
     
 def programming_collective_intelligence(request, pk):
-    context = {
+    context.update({
         'head_title_text': 'programming collective intelligence',
         'breadcrumb': [HOME_BREAD,{'text': 'Article','href': '#'},{'text': 'programming collective intelligence'}],
-    }
+    })
     return render(request, 'article/programming_collective_intelligence/%s.html' % pk, context)
 
 
 def hadoop_deployment(request, pk):
-    context = {
+    context.update({
         'head_title_text': 'hadoop deployment',
         'breadcrumb': [HOME_BREAD,{'text': 'Article','href': '#'},{'text': 'hadoop deployment'}],
-    }
+    })
     return render(request, 'article/hadoop_deployment/%s.html' % pk, context)
 
 def mahout_basic(request, pk):
-    context = {
+    context.update({
         'head_title_text': 'mahout basic',
         'breadcrumb': [HOME_BREAD,{'text': 'Article','href': '#'},{'text': 'mahout basic'}],
-    }
+    })
     return render(request, 'article/mahout_basic/%s.html' % pk, context)
 
 
